ac0dc9dee1153031eb0adc8fd2f2edb94c17d0a2
semiexp <linthya.v@gmail.com>
2017-05-03 21:23:49 +0900

    Add codes for Kakuro

---
+++ js/kk_field.js
@@ -0,0 +1,89 @@
+function KakuroField(problem) {
+    this.height = problem.getHeight();
+    this.width = problem.getWidth();
+    this.field = [];
+    for (var y = 0; y < this.height; ++y) {
+        var line = [];
+        for (var x = 0; x < this.width; ++x) {
+            if (problem.isClue(x, y)) {
+                line.push({
+                    cellType: KakuroField.CELL_CLUE,
+                    clueHorizontal: problem.getClueHorizontal(x, y),
+                    clueVertical: problem.getClueVertical(x, y)
+                });
+            } else {
+                line.push({
+                    cellType: KakuroField.CELL_BLANK,
+                    value: -1
+                });
+            }
+        }
+        this.clue.push(line);
+    }
+}
+KakuroField.CELL_CLUE = 0;
+KakuroField.CELL_BLANK = 1;
+KakuroField.prototype.getHeight = function () {
+    return this.height;
+}
+KakuroField.prototype.getWidth = function () {
+    return this.width;
+}
+KakuroField.prototype.isClue = function (x, y) {
+    return this.field[y][x].cellType == KakuroField.CELL_CLUE;
+}
+KakuroField.prototype.getClueHorizontal = function (x, y) {
+    return this.field[y][x].clueHorizontal;
+}
+KakuroField.prototype.getClueVertical = function (x, y) {
+    return this.field[y][x].clueVertical;
+}
+KakuroField.prototype.isFinished = function () {
+    var field = this.field;
+    var height = this.height;
+    var width = this.width;
+
+    for (var y = 0; y < height; ++y) {
+        var currentValues = [];
+        for (var x = width - 1; x >= 0; --x) {
+            if (field[y][x].cellType == KakuroField.CELL_BLANK) {
+                currentValues.push(field[y][x].value);
+            } else {
+                var expectedSum = field[y][x].clueHorizontal;
+                var currentSum = 0, used = 0;
+                for (var i = 0; i < currentValues.length; ++i) {
+                    var v = currentValues[i];
+                    if (!(1 <= v && v <= 9)) return false;
+                    if ((used & (1 << v)) != 0) return false;
+                    used |= 1 << v;
+                    currentSum += v;
+                }
+                if (expectedSum != currentSum) return false;
+                currentValues = [];
+            }
+        }
+        if (currentValues.length != 0) return false;
+    }
+    for (var x = 0; x < width; ++x) {
+        var currentValues = [];
+        for (var y = height - 1; y >= 0; --y) {
+            if (field[y][x].cellType == KakuroField.CELL_BLANK) {
+                currentValues.push(field[y][x].value);
+            } else {
+                var expectedSum = field[y][x].clueHorizontal;
+                var currentSum = 0, used = 0;
+                for (var i = 0; i < currentValues.length; ++i) {
+                    var v = currentValues[i];
+                    if (!(1 <= v && v <= 9)) return false;
+                    if ((used & (1 << v)) != 0) return false;
+                    used |= 1 << v;
+                    currentSum += v;
+                }
+                if (expectedSum != currentSum) return false;
+                currentValues = [];
+            }
+        }
+        if (currentValues.length != 0) return false;
+    }
+    return true;
+}
---
+++ js/kk_problem.js
@@ -0,0 +1,39 @@
+function KakuroProblem(width, height) {
+    this.height = height;
+    this.width = width;
+    this.clue = [];
+    for (var y = 0; y < height; ++y) {
+        var line = [];
+        for (var x = 0; x < width; ++x) {
+            line.push({
+                cellType: KakuroProblem.CELL_BLANK,
+                clueHorizontal: 0,
+                clueVertical: 0
+            });
+        }
+        this.clue.push(line);
+    }
+}
+KakuroProblem.CELL_CLUE = 0;
+KakuroProblem.CELL_BLANK = 1;
+KakuroProblem.prototype.getHeight = function () {
+    return this.height;
+}
+KakuroProblem.prototype.getWidth = function () {
+    return this.width;
+}
+KakuroProblem.prototype.isClue = function (x, y) {
+    return this.clue[y][x].cellType == KakuroProblem.CELL_CLUE;
+}
+KakuroProblem.prototype.getClueHorizontal = function (x, y) {
+    return this.clue[y][x].clueHorizontal;
+}
+KakuroProblem.prototype.getClueVertical = function (x, y) {
+    return this.clue[y][x].clueVertical;
+}
+KakuroProblem.prototype.setClueHorizontal = function (x, y, v) {
+    this.clue[y][x].clueHorizontal = v;
+}
+KakuroProblem.prototype.setClueVertical = function (x, y, v) {
+    this.clue[y][x].clueVertical = v;
+}
